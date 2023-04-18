c18f783d3609df6ffe9c63d3c67b83d14a5bca09
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2023-04-18 20:18:28 +0800

    adding new color pallete

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -14,37 +14,126 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Obx(
-              () => Text(
-                controller.count.toString(),
-                style: const TextStyle(fontSize: 30),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Primary",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: () => {controller.increment()},
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(20),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: Get.width,
+                height: 50,
+                color: Theme.of(context).colorScheme.primary,
+                child: Center(
+                  child: Text(
+                    "OnPrimary",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
                   ),
                 ),
-                child: const Text(
-                  "Count",
-                  style: TextStyle(fontSize: 15, color: Colors.white),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Primary Container",
+                style: TextStyle(
+                  fontSize: 20,
                 ),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: Get.width,
+                height: 50,
+                color: Theme.of(context).colorScheme.primaryContainer,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Secondary",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: Get.width,
+                height: 50,
+                color: Theme.of(context).colorScheme.secondary,
+                child: Center(
+                  child: Text(
+                    "OnPrimary",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Theme.of(context).colorScheme.onSecondary,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Secondary Container",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: Get.width,
+                height: 50,
+                color: Theme.of(context).colorScheme.secondaryContainer,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Obx(
+                () => Text(
+                  controller.count.toString(),
+                  style: const TextStyle(fontSize: 30),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                onPressed: () => {controller.increment()},
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    "Count",
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/resources/color.dart
+++ lib/resources/color.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 const color = ColorScheme(
   brightness: Brightness.light,
 
-  background: Color(0x123),
-  onBackground: Color(0xFF282A39),
+  background: Color(0xFFFEFEFE),
+  onBackground: Color(0xFF1E293B),
   //tertiary
-  primary: Color(0xFFD14D72),
+  primary: Color(0xFF7d97f3),
   onPrimary: Color(0xFFFFFFFF),
-  primaryContainer: Color.fromARGB(255, 161, 60, 88),
+  primaryContainer: Color(0xFF6684f1),
   //secondary
-  secondary: Color.fromARGB(255, 252, 160, 160),
+  secondary: Color(0xFFf37d96),
   onSecondary: Color(0xFFFFFFFF),
-  secondaryContainer: Color.fromARGB(255, 198, 125, 125),
+  secondaryContainer: Color(0xFFf16683),
   //surface
   surface: Color(0xFFFFFFFF),
-  onSurface: Color(0xFFFFFFFF),
+  onSurface: Color(0xFF334155),
   //error
   error: Color(0xFFE64848),
   onError: Color(0xFFffffff),
