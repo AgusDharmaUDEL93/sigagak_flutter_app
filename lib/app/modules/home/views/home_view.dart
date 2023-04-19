6cd0c1e83c68a8fd1f4893c6c748a4715210313c
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2023-04-19 19:32:22 +0800

    Adding new home-page for aps

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,132 +10,162 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                "Primary",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            child: Container(
+              width: Get.width,
+              height: Get.height * 0.55,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.background,
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: Get.width,
-                height: 50,
-                color: Theme.of(context).colorScheme.primary,
-                child: Center(
-                  child: Text(
-                    "OnPrimary",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Theme.of(context).colorScheme.onPrimary,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/img/logo/provinsiBali.png",
+                    width: 132,
+                    height: 135,
+                  ),
+                  Text(
+                    "SIGAGAK",
+                    style: GoogleFonts.poppins(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.onBackground,
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                "Primary Container",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: Get.width,
-                height: 50,
-                color: Theme.of(context).colorScheme.primaryContainer,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                "Secondary",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: Get.width,
-                height: 50,
-                color: Theme.of(context).colorScheme.secondary,
-                child: Center(
-                  child: Text(
-                    "OnPrimary",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Theme.of(context).colorScheme.onSecondary,
+                  Container(
+                    width: Get.width * 0.8,
+                    child: Text(
+                      "Sistem Pengaduan dan Penanganan Kasus",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300,
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
                     ),
-                  ),
-                ),
+                  )
+                ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                "Secondary Container",
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                width: Get.width,
-                height: 50,
-                color: Theme.of(context).colorScheme.secondaryContainer,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Obx(
-                () => Text(
-                  controller.count.toString(),
-                  style: const TextStyle(fontSize: 30),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextButton(
-                onPressed: () => {controller.increment()},
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).colorScheme.primary,
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(20),
-                    ),
-                  ),
-                  child: const Text(
-                    "Count",
-                    style: TextStyle(fontSize: 15, color: Colors.white),
-                  ),
-                ),
-              )
-            ],
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              width: Get.width,
+              height: Get.height * 0.5,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 50,
+                    child: Center(
+                      child: Container(
+                        height: 4,
+                        width: 114,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 40,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Selamat Datang",
+                            style: GoogleFonts.poppins(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                          ),
+                          Text(
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
+                            textAlign: TextAlign.justify,
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.normal,
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                          ),
+                          const Expanded(
+                            child: SizedBox(),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Container(
+                              width: Get.width,
+                              height: 55,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.surface,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Log in",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    color:
+                                        Theme.of(context).colorScheme.onSurface,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Container(
+                              width: Get.width,
+                              height: 55,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(
+                                  color:
+                                      Theme.of(context).colorScheme.onPrimary,
+                                  width: 2,
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Register",
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    color:
+                                        Theme.of(context).colorScheme.onPrimary,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
