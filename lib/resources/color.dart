2b95ec0d69004b820b242dea8761b2f26b3f17b5
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2023-04-19 18:44:56 +0800

    adding new color pallete

--- lib/resources/color.dart
+++ lib/resources/color.dart
@@ -3,19 +3,19 @@ import 'package:flutter/material.dart';
 const color = ColorScheme(
   brightness: Brightness.light,
 
-  background: Color(0xFFFEFEFE),
+  background: Color(0xFFFFFFFF),
   onBackground: Color(0xFF1E293B),
   //tertiary
-  primary: Color(0xFF7d97f3),
+  primary: Color(0xFF0d6efd),
   onPrimary: Color(0xFFFFFFFF),
-  primaryContainer: Color(0xFF6684f1),
+  primaryContainer: Color(0xFF0262ef),
   //secondary
-  secondary: Color(0xFFf37d96),
-  onSecondary: Color(0xFFFFFFFF),
-  secondaryContainer: Color(0xFFf16683),
+  secondary: Color(0xFFFEE600),
+  onSecondary: Color(0xFF000000),
+  secondaryContainer: Color(0xFFe5cf00),
   //surface
   surface: Color(0xFFFFFFFF),
-  onSurface: Color(0xFF334155),
+  onSurface: Color(0xFF125DB1),
   //error
   error: Color(0xFFE64848),
   onError: Color(0xFFffffff),
